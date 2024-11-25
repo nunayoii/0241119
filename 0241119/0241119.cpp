f74862434a7cb9b59c33b63de5515c22f71f7e7e
nunayoii <4b3g0090@office.stust.edu.tw>
2024-11-25 23:08:54 +0800

    加入專案檔案。

---
+++ 0241119/0241119.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+private:
+    int id;
+    string lastName;
+    string firstName;
+    char gender; 
+    string birthDate;
+
+public:
+ 
+    Person(int id, string lastName, string firstName, char gender, string birthDate) {
+        this->id = id;
+        this->lastName = lastName;
+        this->firstName = firstName;
+        this->gender = gender;
+        this->birthDate = birthDate;
+    }
+
+   
+    void display() {
+        cout << "ID: " << id << endl;
+        cout << "Last Name: " << lastName << endl;
+        cout << "First Name: " << firstName << endl;
+        cout << "Gender: " << (gender == 'M' ? "Male" : "Female") << endl;
+        cout << "Birth Date: " << birthDate << endl;
+    }
+};
+
+int main() {
+    Person person(1, "Smith", "John", 'M', "1990-01-01");
+    person.display();
+
+    return 0;
+}
